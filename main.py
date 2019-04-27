b2579920056b99be16137af8ec937276aba29e64
will li <javenli126@gmail.com>
2019-04-27 17:26:34 +1000

    modified soundex

--- main.py
+++ main.py
@@ -6,8 +6,121 @@ import operator
 import Levenshtein as lv
 import jellyfish as jf
 
+def apply_soundex(misspell, dictionary):
+    count  = 0
+    result = []
+
+    for mis_word in misspell:
+        predict_words = []
+
+        if mis_word not in dictionary:
+            if '/' not in mis_word:
+                for dict_word in dictionary:
+                    soundex_mis  = jf.soundex(mis_word)
+                    soundex_dict = jf.soundex(dict_word)
+                    l_dist       = jf.levenshtein_distance(soundex_mis, soundex_dict)
+
+                    predict_words.append((dict_word, l_dist))
+
+                first_five_pred = sorted(predict_words, key=operator.itemgetter(1), reverse=False)[:5]
+                pred_words = [x[0] for x in first_five_pred]
+
+                result.append(pred_words)
+
+            else:
+                # do not predict when  word contains '/', a lazy method
+                result.append(mis_word)
+
+        # if mis_word in dictionary
+        else:
+            result.append(mis_word)
+        
+        count += 1
+        print("Processing: {} / {}".format(count, len(misspell)), end='\r')
+
+
+    return result
+
+
+def apply_soundex_ngram(misspell, dictionary):
+    count  = 0
+    result = []
+
+    soundex_dict = list(map(jf.soundex, dictionary))
+    soundex_mis  = list(map(lambda x: {x:jf.soundex(x)}, misspell))
+    # print(soundex_mis)
+
+    G = ngram.NGram(soundex_dict)
+
+    for (mis_word, s_mis) in misspell:
+        if mis_word not in dictionary:
+            if '/' not in mis_word:
+                pred_words = []
+                if G.search(s_mis, threshold = 0.4):
+                    search_result = G.search(jf.soundex(mis_word), threshold = 0.4)
+
+                    try:    search_result[0][1]
+                    except: search_result = (mis_word, 1)
+                    else:   highest_score = search_result[0][1]
+                    
+                    for (w,s) in search_result:
+                        if math.isclose(s, highest_score):
+                            pred_words.append(w)
+
+                    if len(pred_words) == 1:
+                        result.append(pred_words[0])
+                    else:
+                        result.append(pred_words)
+
+            else:
+                # do not predict when  word contains '/', a lazy method
+                result.append(mis_word)
+
+        # if mis_word in dictionary
+        else:
+            result.append(mis_word)
+        
+        count += 1
+        print("Processing: {} / {}".format(count, len(misspell)), end='\r')
+
+    return result
+
+
 def apply_damerau_levenshtein(misspell, dictionary):
-    pass
+    count  = 0
+    result = []
+
+    for mis_word in misspell:
+        predict_words = []
+
+        if mis_word not in dictionary:
+            if '/' not in mis_word:
+                for dict_word in dictionary:
+                    # dl_dist = jf.damerau_levenshtein_distance(mis_word, dict_word)
+                    # jaro_dist = jf.jaro_distance(mis_word, dict_word)
+                    # jw_dist = jf.jaro_winkler(mis_word, dict_word)
+                    hamming_dist = jf.hamming_distance(mis_word, dict_word)
+
+                    predict_words.append((dict_word, hamming_dist))
+
+                first_five_pred = sorted(predict_words, key=operator.itemgetter(1), reverse=False)[:5]
+                pred_words = [x[0] for x in first_five_pred]
+
+                result.append(pred_words)
+
+            else:
+                # do not predict when  word contains '/', a lazy method
+                result.append(mis_word)
+
+        # if mis_word in dictionary
+        else:
+            result.append(mis_word)
+        
+        count += 1
+        print("Processing: {} / {}".format(count, len(misspell)), end='\r')
+    
+    return result
+
 
 def apply_lev_dist(misspell, dictionary):
 
@@ -97,24 +210,23 @@ def apply_ngram(misspell, dictionary):
 
 
 def predict(misspell, dictionary, method):
-    if method == 'ngram':
-        result = apply_ngram(misspell, dictionary)
-    elif method == 'soundx':
-        pass
-    elif method == 'levenshtein_distance':
-        result = apply_lev_dist(misspell, dictionary)
-    elif method == 'weighted_levenshtein_distance':
-        result = apply_damerau_levenshtein(misspell, dictionary)
+    if method == 'ngram':                           result = apply_ngram(misspell, dictionary)
+    elif method == 'soundex':                       result = apply_soundex(misspell, dictionary)
+    elif method == 'soundex ngram':                 result = apply_soundex_ngram(misspell, dictionary)
+    elif method == 'levenshtein_distance':          result = apply_lev_dist(misspell, dictionary)
+    elif method == 'damerau_levenshtein_distance':  result = apply_damerau_levenshtein(misspell, dictionary)
     elif method == 'editx':
         pass
 
     return result
 
+
 def main(argv):
     option = argv[1]
     option_dict = {
         'n'     : 'ngram',
-        's'     : 'soundx',
+        's'     : 'soundex',
+        'sn'    : 'soundex ngram',
         'ld'    : 'levenshtein_distance',
         'dld'   : 'damerau_levenshtein_distance',
         'e'     : 'editx'
